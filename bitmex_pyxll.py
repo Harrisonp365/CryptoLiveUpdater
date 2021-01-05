792ed8fc5060d99bb9a5d613c8d0ce9d49974df6
Harrisonp365 <harrisonpowell365@gmail.com>
2021-01-06 10:55:05 +1100

    code now works, had a few small errors. Initial start up is slow, once connected it is much faster

--- bitmex_pyxll.py
+++ bitmex_pyxll.py
@@ -1,8 +1,10 @@
 from pyxll import xl_func, get_event_loop, RTD
 from bitmex import BitMex
 
+
 class BitMexRTD(RTD):
-    #use single Bitmex obj for all func
+
+    # Use a single BitMex object for all RTD functions
     _bitmex = BitMex(loop=get_event_loop())
 
     def __init__(self, symbol, field):
@@ -11,13 +13,17 @@ class BitMexRTD(RTD):
         self.__field = field
 
     async def connect(self):
-        await self.bitmex.subscribe(self.__symbol, self.__field, self.__update)
+        # Subscribe to BitMix updates when Excel connects to the RTD object
+        await self._bitmex.subscribe(self.__symbol, self.__field, self.__update)
 
     async def disconnect(self):
+        # Unsubscribe to BitMix updates when Excel disconnects from the RTD object
         await self._bitmex.unsubscribe(self.__symbol, self.__field, self.__update)
 
-        async def __update(self, symbol, field, value, timestamp):
-            self.value = value
+    async def __update(self, symbol, field, value, timestamp):
+        # Update the value in Excel
+        self.value = value
+
 
 @xl_func("string symbol, string field: rtd")
 def bitmex_rtd(symbol, field):
